cd9baa159344c3724bb998fdfc0568b80b789100
Nicessly <nicessly3@gmail.com>
2024-04-15 21:27:14 -0300

    bien ahora

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
 import Register from './pages/register/register';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 
 
 function App() {
   const [count, setCount] = useState(0);
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
 
   return (
     <>
